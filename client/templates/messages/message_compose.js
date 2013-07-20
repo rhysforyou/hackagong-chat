6d753fa90c51121b92ca362b7abd4a3c819f71d3
Rhys Powell <rhys@rpowell.me>
2013-07-20 19:18:18 +1000

    Return key now posts messages

--- client/templates/messages/message_compose.js
+++ client/templates/messages/message_compose.js
@@ -1,18 +1,28 @@
 Template.messageCompose.events({
   'submit form#new-message': function(event) {
-    event.preventDefault()
+    submitForm(event)
+  },
 
-    var body = $(event.target).find("textarea").val()
-    var message = {
-      body: body,
-      roomId: Session.get("currentRoomId")
+  'keydown': function(event) {
+    if (event.keyCode === 13 && !event.shiftKey) {
+      submitForm(event)
     }
-
-    Meteor.call('message', message, function(error, response) {
-      if (!error)
-        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-    })
-
-    $(event.target).find("textarea").val('')
   }
 })
+
+function submitForm(event) {
+  event.preventDefault()
+
+  var body = $("form#new-message textarea").val()
+  var message = {
+    body: body,
+    roomId: Session.get("currentRoomId")
+  }
+
+  Meteor.call('message', message, function(error, response) {
+    if (!error)
+      $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+  })
+
+  $("form#new-message textarea").val('')
+}
