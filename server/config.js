e8ec305165af4d541f55e173a9ef171c4233d442
Rhys Powell <rhys@rpowell.me>
2013-07-21 11:48:52 +1000

    Configure email in production

---
+++ server/config.js
@@ -0,0 +1,11 @@
+if (process.env.MAILGUN_SMTP_SERVER) {
+  // Set up our SMTP server
+  var smtpAddress  = process.env.MAILGUN_SMTP_SERVER
+  var smtpPort     = process.env.MAILGUN_SMTP_PORT
+  var smtpUsername = process.env.MAILGUN_SMTP_LOGIN
+  var smtpPassword = process.env.MAILGUN_SMTP_PASSWORD
+
+  process.env.MAIL_URL = "smtp://" + smtpUsername + ":" + smtpPassword + "@" + smtpAddress + ":" + smtpPort
+} else {
+  console.log("Running email in development mode")
+}
