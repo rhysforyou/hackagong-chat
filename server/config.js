8fa911dfeb7cc268b52efa8b876a4572275870ab
Rhys Powell <rhys@rpowell.me>
2013-07-21 12:03:50 +1000

    SMTP

--- server/config.js
+++ server/config.js
@@ -2,8 +2,8 @@ if (process.env.MAILGUN_SMTP_SERVER) {
   // Set up our SMTP server
   var smtpAddress  = process.env.MAILGUN_SMTP_SERVER
   var smtpPort     = process.env.MAILGUN_SMTP_PORT
-  var smtpUsername = process.env.MAILGUN_SMTP_LOGIN
-  var smtpPassword = process.env.MAILGUN_SMTP_PASSWORD
+  var smtpUsername = encodeURIComponent(process.env.MAILGUN_SMTP_LOGIN)
+  var smtpPassword = encodeURIComponent(process.env.MAILGUN_SMTP_PASSWORD)
 
   process.env.MAIL_URL = "smtp://" + smtpUsername + ":" + smtpPassword + "@" + smtpAddress + ":" + smtpPort
   console.log("Sending emails to " + process.env.MAIL_URL)
